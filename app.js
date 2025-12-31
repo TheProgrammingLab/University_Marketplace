1e825187fb3536f6c87052106cb245a29ecaf744
Kareem Taiye <olayinkahassan117@gmail.com>
2025-12-31 10:58:59 +0000

    added auth route handler in app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from "express";
 import cors from "cors";
 
 import globalErrHandler from "./middlewares/globalErrorHandler.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/auth", authRouter);
 
 app.use(globalErrHandler);
 export default app;
