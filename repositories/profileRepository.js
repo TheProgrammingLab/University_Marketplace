a0f0781f7c8f906ca581e66c91e3a0062292784b
Kareem Taiye <olayinkahassan117@gmail.com>
2026-01-24 03:43:10 +0000

    get /profile  endpoint implementedt

--- repositories/profileRepository.js
+++ repositories/profileRepository.js
@@ -9,4 +9,16 @@ export default class ProfileRepository {
 
     await db.query(query, [userId]);
   }
+
+  static async getUserProfile(userId, db = pool) {
+    const query = `
+    SELECT u.id, u.first_name, u.last_name, u.username, u.email, p.avatar_url, p.phone
+    FROM users u
+    INNER JOIN user_profiles p ON p.user_id = u.id
+    WHERE u.id = $1
+    `;
+
+    const { rows } = await db.query(query, [userId]);
+    return rows[0] || null;
+  }
 }
